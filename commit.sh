39db22df5a3f0e90e5fc450bb4c4958b71cf5f3d
pkemb <pkemb@outlook.com>
2021-07-10 22:34:51 +0800

    修复commit脚本运行错误

--- commit.sh
+++ commit.sh
@@ -3,11 +3,11 @@
 current_year=$(date +"%Y")
 stat_file="page_puv_statistics_${current_year}.json"
 if [ -f "$stat_file" ]; then
-    commit_msg=$(cat $stat_file | tr '[,{}]' '\n' | grep time | c cut -d"\"" -f4 | sort -n | tail -n1)
+    commit_msg=$(cat $stat_file | tr '[,{}]' '\n' | grep time | cut -d"\"" -f4 | sort -n | tail -n1)
 else
     commit_msg=$(date +"%Y-%m-%d %H:%M:%S")
 fi
 
 git add -A
-git commit --allow-empty -m $commit_msg
+git commit --allow-empty -m "date: $commit_msg"
 git push origin master
